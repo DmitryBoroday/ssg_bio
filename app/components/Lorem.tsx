e1520f0b8da6f77354bdeba68df6e303b95c867d
unknown <dboroday2018@gmail.com>
2025-12-28 18:12:43 +0200

    more copywritng

--- app/components/Lorem.tsx
+++ app/components/Lorem.tsx
@@ -10,7 +10,7 @@ export function LoremIpsum() {
 
     <article>
       <h2 className='indent-6 text-lg'>
-        <code className='font-bold text-gray-200'>
+        <code className='font-bold text-gray-200 text-xl'>
           Starting coding in 2019, I was building Web Apps for private clients using JavaScript technologies feeling myself pretty comfortable with React ecosystem and it seems like business likes it as well!
         </code>
       </h2>
@@ -20,15 +20,16 @@ export function LoremIpsum() {
           <h2 className='text-blue-300 font-bold felx flex row items-center justify-end gap-1 hover:text-gray-400 transition-all'>React intro <RxVideo size={20} /></h2> </Link>
       </div>
       <h3 className='indent-6 text-lg'>
-        <code className='font-bold text-gray-200'>
+        <code className='font-bold text-gray-200 text-xl'>
           Following the latest trends as the world of web development is constantly evolving, we design, create, and optimize websites and applications in response to customers needs, technological advancements, and the demands of the growing industry, which is changing rapidly, dictating new trends that are very exciting and have a great future! So, the industry is growing, and we have a lot of work to do...
         </code>
       </h3>
       <p className='text-blue-300 font-bold pt-12 pb-4'>My Stack</p>
       <Stack />
       <h4 className='indent-6'>
-        <code className='font-bold text-gray-200'>
-          As you can see from my core stack - it's pretty basic and I believe it's production ready! Of course, these are some of the most used technologies if we talk MERN stack, and we have much more out there... To put it briefly, I try not to overcomplicate things and stick close to the official documentation, following proven patterns! But we are all learning and my goal is to get deeper into the development process by working on interesting projects!
+        <code className='font-bold text-gray-200 text-xl'>
+          As you can see from my core stack - it's pretty basic and I believe it's production ready! Of course, these are some of the most used technologies if we talk <span>
+            <a href='https://www.mongodb.com/resources/languages/mern-stack' target='_blank'><code className='text-blue-300 font-bold'>MERN</code></a></span> stack, and we have much more out there... To put it briefly, I try not to overcomplicate things and stick close to the official documentation, following proven patterns!
         </code>
       </h4>
       <div className='flex items-center justify-between'>
@@ -37,8 +38,10 @@ export function LoremIpsum() {
           <p className='text-blue-300 font-bold felx flex row items-center justify-end mt-10 hover:text-gray-400 transition-all'>Blog<IoIosArrowRoundForward size={30} /></p>
         </Link>
       </div>
-      <h6 className='indent-10'>
-        At the end of the day we got a lot! Developers wraps their heads around the emergence of many new trends! We have Mobile-First approach with Accelerated Mobile Pages (AMP) powered by AI-Chatbots and Push Notifications! Our PWAs has more accessibility with Voice UI and they looks nice with Dark Mode and smooth Motion-UI.The API-first approach, Serverless Architecture with Static Website Generators tooks place. With help of TypeScript and new Meta-Framworks like NextJS we have a lot of interesting job to do ,guys!
+      <h6 className='indent-6'>
+        <code className='text-xl text-gray-200 font-bold'>
+          At the end of the day, we got a lot to wrap our heads around! Mobile-First approach with Accelerated Mobile Pages (AMP) powered by AI-Chatbots and Push Notifications or PWAs with Voice сontrol. The API-first approach, Serverless Architecture with Static Website Generators took place. With the help of TypeScript and new Meta-Framworks like NextJS, we have a lot of interesting jobs to do!
+        </code>
       </h6>
       <p className='text-blue-300 font-bold felx flex row items-center justify-start pt-12 pb-2'>Message me</p>
       <Form />
--- app/video/page.tsx
+++ app/video/page.tsx
@@ -6,19 +6,18 @@ export default function Home() {
   return (
     <article>
       <h1 className='sm:mt-20 am:mb-15 mt-5 mb-10'>
-          <code className='text-indigo-300'>top industry guys </code> talking React )
+        <code className='text-indigo-300'>top industry guys </code> talking React )
       </h1>
-        <div className=' flex items-center  justify-between h-10'>    
-          <Link href='/'>
-          <h2 className='font-bold hover:text-gray-500 transition-all flex flex-row items-center'><IoIosArrowRoundBack size={30} />back</h2></Link>  
-          <div>
-          </div>
-        </div>               
-      <VideoPlayer /> 
-         <p className='indent-10'>
+      <div className=' flex items-center  justify-between h-10'>
+        <Link href='/'>
+          <h2 className='font-bold hover:text-gray-500 transition-all flex flex-row items-center'><IoIosArrowRoundBack size={30} />back</h2></Link>
+        <div>
+        </div>
+      </div>
+      <VideoPlayer />
+      <code className='indent-10 text-gray-200 text-xl'>
         I think that the idea of React framework was so good that it was immediately picked up by developers and businesses. Some people hate it, other loves this technology! But one thing is clear - React has paved its way in the history of Web Development and I hope we will see many more great products built on top of this ecosystem...
-        </p>
-   
+      </code>
     </article>
   )
 }
