9522fce1d0f2824d919c6b8f20a64fc7166342ef
unknown <dboroday2018@gmail.com>
2026-04-29 13:58:16 +0300

    ui polish

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
     return (
         <footer className='p-12 border-t border-zinc-900 teachersBg text-gray-400 font-bold text-sm'>
             <p> ©2019 Dmitry Boroday</p>
-
         </footer>
     )
 }
