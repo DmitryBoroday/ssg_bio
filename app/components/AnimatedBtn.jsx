eed507f254a8beea472ecfd774912a6369802017
unknown <dboroday2018@gmail.com>
2026-05-19 14:48:02 +0300

    add video

--- app/components/AnimatedBtn.jsx
+++ app/components/AnimatedBtn.jsx
@@ -35,8 +35,8 @@ function Box() {
         preload='auto'
         width='100%'
         height='100%'
-        poster=''
-        src={'video/bg.mp4'}
+        poster={'/videos/big_cut.avif'}
+        src={'video/big_cut.mp4'}
       />
     </motion.div>
   )
@@ -46,38 +46,6 @@ export default function AnimatedBtn() {
   const [show, setShow] = useState(false)
   return (
     <div className='w-full h-full'>
-      {/* <div className='rounded-full flex flex-row justify-around gap-2  bg-gradient-to-r from-indigo-500 to-cyan-500'>
-        <div className='w-16 h-16 p-1'>
-          <video
-            className='h-full w-full filter brightness-125 object-cover rounded-full'
-            loop
-            autoPlay
-            muted
-            playsInline
-            preload='auto'
-            width='100%'
-            height='100%'
-            poster='/videos/popularPoster.avif'
-            src={'video/bg.mp4'}
-          />
-        </div>
-        <div className=' flex flex-col justify-center pr-4 '>
-          <h3 className='text-bold-xl'>Dmitry Boroday</h3>
-          <hr className='text-white' />
-          <h4 className='text-bold'>Web Developer</h4>
-        </div>
-        <div className=' flex flex-col justify-center pr-4'>
-          <a
-            href='https://github.com/DmitryBoroday?tab=repositories'
-            target='_blank'
-          >
-            <FaGithub
-              size={34}
-              className='hover:text-black transition-all'
-            />
-          </a>
-        </div>
-      </div> */}
       <div className='text-2xl text-white fixed right-2 lg:right-8 bottom-6 w-16 h-16 rounded-full bg-red-400 z-50 shadow-md'>
         <motion.button
           whileTap={{ scale: 0.95 }}
